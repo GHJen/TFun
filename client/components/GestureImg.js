76bfb459835e905732800971449b43760c6f7907
Jennifer Nugent <jmn2158@columbia.edu>
2020-09-29 15:34:10 -0700

    route fix

--- client/components/GestureImg.js
+++ client/components/GestureImg.js
@@ -1,4 +1,4 @@
 import React from 'react';
 
-const gestureImg = (props) => <img src={`/${props.gesture}.png`} />
+const gestureImg = (props) => <img src={`/GestureImgs/${props.gesture}.png`} />
 export default gestureImg;
