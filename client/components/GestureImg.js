7e018fdaf64e035159695b6d1f119aa7410634b8
Jennifer Nugent <jmn2158@columbia.edu>
2020-09-29 15:52:15 -0700

    img size

--- client/components/GestureImg.js
+++ client/components/GestureImg.js
@@ -1,4 +1,4 @@
 import React from 'react';
 
-const gestureImg = (props) => <img src={`/GestureImgs/${props.gesture}.png`} />
+const gestureImg = (props) => <img className='gestureImg' src={`/GestureImgs/${props.gesture}.png`} />
 export default gestureImg;
